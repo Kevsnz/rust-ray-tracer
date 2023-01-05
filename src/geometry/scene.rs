0949e8d7c88cf4aa537f520ea83b4c6a9e6c030a
Kevsnz <15689588+Kevsnz@users.noreply.github.com>
2023-01-05 13:16:29 +0500

    Diffuse color tracing implementation

--- src/geometry/scene.rs
+++ src/geometry/scene.rs
@@ -1,16 +1,22 @@
-use super::{sphere::Sphere, vector::Vector};
+use super::{point_light::PointLight, sphere::Sphere, vector::Vector};
 
 pub struct Scene {
     pub spheres: Vec<Sphere>,
+    pub lights: Vec<PointLight>,
 }
 
 impl Scene {
     pub fn new() -> Scene {
         let spheres = vec![
-            Sphere::new(Vector::new(0.0, 0.0, 3.0), 1.0, Vector::new(2.0, 1.0, 0.0)),
+            Sphere::new(Vector::new(0.0, 0.0, 3.0), 1.0, Vector::new(1.0, 0.5, 0.0)),
             Sphere::new(Vector::new(1.0, 1.0, 4.0), 0.75, Vector::new(0.0, 0.0, 1.0)),
         ];
 
-        Scene { spheres }
+        let lights = vec![
+            PointLight::new(Vector::new(2.0, 3.0, 2.0), Vector::new(0.25, 0.5, 0.75)),
+            PointLight::new(Vector::new(-1.0, 0.0, 1.0), Vector::new(0.75, 0.0, 0.25)),
+        ];
+
+        Scene { spheres, lights }
     }
 }
--- src/tracer.rs
+++ src/tracer.rs
@@ -1,4 +1,5 @@
 use crate::camera::Camera;
+use crate::geometry::point_light::PointLight;
 use crate::geometry::scene::Scene;
 use crate::geometry::sphere::Sphere;
 use crate::geometry::vector::Vector;
@@ -17,14 +18,37 @@ impl Tracer {
 
         let closest_intersect = Self::closest_intersect(camera.pos, dir, &scene.spheres);
 
-        if let Some((sph, t)) = closest_intersect {
-            let c = 1.0 - t * t / (sph.center - camera.pos).len_sq();
-            return (c * sph.color.x, c * sph.color.y, c * sph.color.z);
+        if let None = closest_intersect {
+            let x = x / 2.0 + 0.5;
+            let y = y / 2.0 + 0.5;
+            return (x % 1.0, y % 1.0, (x + y) % 1.0);
         }
 
-        let x = x / 2.0 + 0.5;
-        let y = y / 2.0 + 0.5;
-        (x % 1.0, y % 1.0, (x + y) % 1.0)
+        let (sph, t) = closest_intersect.unwrap();
+
+        let ip = camera.pos + dir * t;
+        let diff_color = Tracer::trace_to_lights(ip, sph.normal(ip), &scene.lights);
+        //let c = 1.0 - t * t / (sph.center - camera.pos).len_sq();
+        (
+            diff_color.x * sph.color.x,
+            diff_color.y * sph.color.y,
+            diff_color.z * sph.color.z,
+        )
+    }
+
+    fn trace_to_lights(ip: Vector, normal: Vector, lights: &Vec<PointLight>) -> Vector {
+        let mut total_color = Vector::zero();
+
+        for light in lights {
+            let to_light = light.pos - ip;
+            let diffuse_coeff = to_light.dot(&normal);
+            if diffuse_coeff <= 0.0 {
+                continue;
+            }
+            total_color += light.color * diffuse_coeff;
+        }
+
+        total_color
     }
 
     fn closest_intersect(
